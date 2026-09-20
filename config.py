a939c983e4513f51c08b6ab615ff4f3668d99bd4
camilaluango <mariacamilaluango@gmail.com>
2026-09-20 14:54:51 -0500

    segundo commit

--- config.py
+++ config.py
@@ -15,22 +15,12 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 class Config:
     """Configuración base (desarrollo)."""
 
-    SECRET_KEY =  os.environ.get("SECRET_KEY", "valor-por-defecto")
+    SECRET_KEY = os.environ.get("SECRET_KEY", "valor-por-defecto")
 
-    # URI de conexión a la base de datos.
-    # Para SQLite el formato es: sqlite:///<ruta-absoluta-al-archivo>
-    # El archivo .db se creará dentro de la carpeta instance/
-    # TODO 2: Verifica que la ruta apunte a instance/tienda.db.
-    #         Si cambias el nombre del archivo, actualízalo aquí.
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(
         BASE_DIR, "instance", "tienda.db"
     )
 
-    # Desactiva un sistema de eventos de SQLAlchemy que no usamos y
-    # que consume memoria innecesariamente.
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # TODO 3 (opcional): pon esta opción en True para ver en la terminal
-    # el SQL que SQLAlchemy genera. Es muy útil para entender qué hace
-    # el ORM por debajo. Desactívala cuando ya no la necesites.
     SQLALCHEMY_ECHO = False
